809ff5c415ccee5496a449f8fef1d6dd08461a7a
Aurimas Blažulionis <0x60@pm.me>
2021-01-04 09:07:37 +0000

    Code for Episode 11 and bit of 12

--- scanflow/src/main.rs
+++ scanflow/src/main.rs
@@ -1,6 +1,7 @@
 use memflow::connector::inventory::ConnectorInventory;
 use memflow::error::*;
 use memflow::mem::virt_mem::VirtualMemory;
+use memflow::types::Address;
 
 use memflow_win32::win32::{Kernel, Win32Process};
 use memflow_win32::{Error, Result};
@@ -8,6 +9,8 @@ use memflow_win32::{Error, Result};
 use simplelog::{Config, LevelFilter, TermLogger, TerminalMode};
 
 use std::convert::TryInto;
+use std::sync::mpsc::{channel, Receiver};
+use std::thread;
 use std::time::Instant;
 
 pub mod value_scanner;
@@ -153,6 +156,16 @@ fn main() -> Result<()> {
                     );
                 }
             }
+            "write" | "wr" => {
+                if let Err(e) = write_value(
+                    args,
+                    &typename,
+                    value_scanner.matches(),
+                    &mut process.virt_mem,
+                ) {
+                    println!("Error: {}", e);
+                }
+            }
             _ => {
                 if let Some((buf, t)) = parse_input(line, &typename) {
                     buf_len = buf.len();
@@ -190,17 +203,100 @@ pub fn print_matches<V: VirtualMemory>(
     Ok(())
 }
 
-pub fn get_line() -> std::result::Result<String, std::io::Error> {
+pub fn get_line() -> std::io::Result<String> {
     let mut output = String::new();
     std::io::stdin().read_line(&mut output).map(|_| output)
+}
+
+pub fn async_get_line() -> Receiver<std::io::Result<String>> {
+    let (tx, rx) = channel();
+    thread::spawn(move || tx.send(get_line()).unwrap());
+    rx
+}
+
+pub fn write_value(
+    args: &str,
+    typename: &Option<String>,
+    matches: &[Address],
+    mut virt_mem: impl VirtualMemory,
+) -> Result<()> {
+    if matches.is_empty() {
+        return Err(Error::Other("no matches found!"));
+    }
+
+    let usage = Error::Other("usage: wr {{idx/*}} {{o/c}} {{value}}");
+    let mut words = args.splitn(3, " ");
+    let (idx, mode, value) = (
+        words.next().ok_or(usage)?,
+        words.next().ok_or(usage)?,
+        words.next().ok_or(usage)?,
+    );
+
+    let (skip, take) = if idx == "*" {
+        (0, matches.len())
+    } else {
+        (
+            idx.parse::<usize>()
+                .map_err(|_| Error::Other("failed to parse index!"))?,
+            1,
+        )
+    };
+
+    let gl = match mode {
+        "o" => Ok(None),
+        "c" => Ok(Some(async_get_line())),
+        _ => Err(Error::Other("failed to parse mode!")),
+    }?;
+
+    let (v, _) = parse_input(value, typename).ok_or(Error::Other("failed to parse value!"))?;
+
+    println!("Write to matches {}-{}", skip, skip + take - 1);
+
+    loop {
+        for &m in matches.iter().skip(skip).take(take) {
+            virt_mem.virt_write_raw(m, v.as_ref()).data_part()?;
+        }
+
+        if let Some(try_get_line) = &gl {
+            if let Ok(ret) = try_get_line.try_recv() {
+                if let Err(e) = ret {
+                    println!("Error reading line: {}", e.to_string());
+                }
+                break;
+            }
+        } else {
+            break;
+        }
+    }
+
+    println!("Write done");
+
+    Ok(())
 }
 
 pub fn print_value(buf: &[u8], typename: &str) -> Option<String> {
     match typename {
         "str" => Some(String::from_utf8_lossy(buf).to_string()),
-        //"str_u16" => print!("{}", String::from_utf16_lossy(buf)),
+        "str_utf16" => {
+            let mut vec = vec![];
+            for w in buf.chunks_exact(2) {
+                let s = u16::from_ne_bytes(w.try_into().unwrap());
+                vec.push(s);
+            }
+            Some(format!("{}", String::from_utf16_lossy(&vec)))
+        }
+        "i128" => Some(format!("{}", i128::from_ne_bytes(buf.try_into().ok()?))),
         "i64" => Some(format!("{}", i64::from_ne_bytes(buf.try_into().ok()?))),
         "i32" => Some(format!("{}", i32::from_ne_bytes(buf.try_into().ok()?))),
+        "i16" => Some(format!("{}", i16::from_ne_bytes(buf.try_into().ok()?))),
+        "i8" => Some(format!("{}", i8::from_ne_bytes(buf.try_into().ok()?))),
+        "u128" => Some(format!("{}", u128::from_ne_bytes(buf.try_into().ok()?))),
+        "u64" => Some(format!("{}", u64::from_ne_bytes(buf.try_into().ok()?))),
+        "u32" => Some(format!("{}", u32::from_ne_bytes(buf.try_into().ok()?))),
+        "u16" => Some(format!("{}", u16::from_ne_bytes(buf.try_into().ok()?))),
+        "u8" => Some(format!("{}", u8::from_ne_bytes(buf.try_into().ok()?))),
+        "f64" => Some(format!("{}", f64::from_ne_bytes(buf.try_into().ok()?))),
+        "f32" => Some(format!("{}", f32::from_ne_bytes(buf.try_into().ok()?))),
         _ => None,
     }
 }
@@ -222,8 +318,18 @@ pub fn parse_input(input: &str, opt_typename: &Option<String>) -> Option<(Box<[u
             }
             Some(out.into_boxed_slice())
         }
+        "i128" => Some(Box::from(value.parse::<i128>().ok()?.to_ne_bytes())),
         "i64" => Some(Box::from(value.parse::<i64>().ok()?.to_ne_bytes())),
         "i32" => Some(Box::from(value.parse::<i32>().ok()?.to_ne_bytes())),
+        "i16" => Some(Box::from(value.parse::<i16>().ok()?.to_ne_bytes())),
+        "i8" => Some(Box::from(value.parse::<i8>().ok()?.to_ne_bytes())),
+        "u128" => Some(Box::from(value.parse::<u128>().ok()?.to_ne_bytes())),
+        "u64" => Some(Box::from(value.parse::<u64>().ok()?.to_ne_bytes())),
+        "u32" => Some(Box::from(value.parse::<u32>().ok()?.to_ne_bytes())),
+        "u16" => Some(Box::from(value.parse::<u16>().ok()?.to_ne_bytes())),
+        "u8" => Some(Box::from(value.parse::<u8>().ok()?.to_ne_bytes())),
+        "f64" => Some(Box::from(value.parse::<f64>().ok()?.to_ne_bytes())),
+        "f32" => Some(Box::from(value.parse::<f32>().ok()?.to_ne_bytes())),
         _ => None,
     }?;
     Some((b, typename.to_string()))
